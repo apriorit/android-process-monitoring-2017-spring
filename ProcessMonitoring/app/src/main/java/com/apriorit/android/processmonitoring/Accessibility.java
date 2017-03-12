a7d324ca39ae5f9237fd8ca31c4e183ca970326b
muriy4328 <musienko_html@mail.ru>
2017-03-12 21:31:33 +0300

    add lock logic

---
+++ ProcessMonitoring/app/src/main/java/com/apriorit/android/processmonitoring/Accessibility.java
@@ -0,0 +1,45 @@
+package com.apriorit.android.processmonitoring;
+
+import android.accessibilityservice.AccessibilityService;
+import android.content.Intent;
+import android.util.Log;
+import android.view.accessibility.AccessibilityEvent;
+
+
+public class Accessibility extends AccessibilityService{
+
+    static final String TAG = "RecorderService";
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        Log.d(TAG,"StartEvent");
+        Log.d(TAG, String.format("packageName: %s  className %s eventType %s" , event.getPackageName(), event.getClassName(), event.getEventType()));
+        startLock();
+
+    }
+
+    @Override
+    public void onInterrupt() {
+        Log.d(TAG, "onInterrupt");
+    }
+
+    @Override
+    protected void onServiceConnected() {
+        super.onServiceConnected();
+        Log.d(TAG, "onServiceConnected");
+    }
+
+
+    //запускаем окно блокировки
+    public void startLock(){
+        try {
+            Intent intent = new Intent(Accessibility.this, Lock.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
