a4c59c253b818df5767f7c6bdc9c087fa6b79958
muriy4328 <musienko_html@mail.ru>
2017-03-19 17:57:48 +0300

    Seve master key on android

--- ProcessMonitoring/app/src/main/java/com/apriorit/android/processmonitoring/Lock.java
+++ ProcessMonitoring/app/src/main/java/com/apriorit/android/processmonitoring/Lock.java
@@ -1,18 +1,34 @@
 package com.apriorit.android.processmonitoring;
 
 import android.app.Instrumentation;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Lock extends AppCompatActivity {
-
+    public static final String APP_PREFERENCES = "preference";
+    public static final String APP_PREFERENCES_KEY = "masterKey";
+    SharedPreferences masterKey;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        masterKey = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lock);
+        final TextView testKey = (TextView) findViewById(R.id.testKey);
+        Button unlock = (Button) findViewById(R.id.unlock);
+        unlock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                testKey.setText(masterKey.getString(APP_PREFERENCES_KEY,"0000"));
+            }
+        });
     }
 
     @Override
