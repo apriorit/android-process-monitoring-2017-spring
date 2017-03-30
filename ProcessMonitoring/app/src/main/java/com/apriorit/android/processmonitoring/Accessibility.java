784745074fc0212891ef984c5298711cff7ba674
muriy4328 <musienko_html@mail.ru>
2017-03-30 14:33:51 +0400

    Added a lock for unistaling the application and disabling the service

--- ProcessMonitoring/app/src/main/java/com/apriorit/android/processmonitoring/Accessibility.java
+++ ProcessMonitoring/app/src/main/java/com/apriorit/android/processmonitoring/Accessibility.java
@@ -1,6 +1,7 @@
 package com.apriorit.android.processmonitoring;
 
 import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.AccessibilityServiceInfo;
 import android.content.Intent;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
@@ -13,7 +14,9 @@ public class Accessibility extends AccessibilityService{
     static final String TAG = "RecorderService";
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        Log.d(TAG,"StartEvent");
+        String app_name =  getString(R.string.app_name);
+        String accessibility_service_label =  getString(R.string.accessibility_service_label);
+        Log.d(TAG,accessibility_service_label);
         List<CharSequence> wordsInWindow = event.getText();
         Log.d(TAG, String.format("packageName: %s  className %s eventType %s text %s" , event.getPackageName(), event.getClassName(), event.getEventType(),event.getText()));
        String nameActivity = (String) wordsInWindow.get(0);
@@ -21,12 +24,11 @@ public class Accessibility extends AccessibilityService{
         String eventPackage = String.valueOf(event.getPackageName());
         Log.d(TAG, eventPackage);
         if(!eventPackage.equals("com.android.settings")
-                ||nameActivity.equals("Accessibility")
-                ||nameActivity.equals("ProcessMonitoring")
+                ||nameActivity.equals(accessibility_service_label)
+                ||nameActivity.equals(app_name)
                 )
         {
             startLock();
-
         }
 
     }
@@ -39,10 +41,15 @@ public class Accessibility extends AccessibilityService{
     @Override
     protected void onServiceConnected() {
         super.onServiceConnected();
+        AccessibilityServiceInfo info = new AccessibilityServiceInfo();
+        info.eventTypes = AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED;
+        info.packageNames = new String[]
+                {"com.example.admin.event", "com.android.settings" ,"com.android.packageinstaller"};
+        info.feedbackType = AccessibilityServiceInfo.FEEDBACK_SPOKEN;
+        info.notificationTimeout = 100;
+        this.setServiceInfo(info);
         Log.d(TAG, "onServiceConnected");
     }
-
-
     //запускаем окно блокировки
     public void startLock(){
         try {
