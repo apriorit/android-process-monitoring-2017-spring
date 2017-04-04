517455e01f7569b4b8174dcc7f5f2ee25f54882a
muriy43 <muriy43@gmail.com>
2017-04-04 20:23:45 +0400

    Software definition of packageName settings

--- ProcessMonitoring/app/src/main/java/com/apriorit/android/processmonitoring/Accessibility.java
+++ ProcessMonitoring/app/src/main/java/com/apriorit/android/processmonitoring/Accessibility.java
@@ -3,6 +3,10 @@ package com.apriorit.android.processmonitoring;
 import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.AccessibilityServiceInfo;
 import android.content.Intent;
+import android.content.pm.PackageInstaller;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 
@@ -10,7 +14,23 @@ import java.util.List;
 
 
 public class Accessibility extends AccessibilityService{
-
+    private String querySettingPkgName(){
+        Intent intent = new Intent(Settings.ACTION_SETTINGS);
+        List<ResolveInfo> resolveInfos = getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        if(resolveInfos == null || resolveInfos.size() == 0){
+            return "";
+        }
+        return resolveInfos.get(0).activityInfo.packageName;
+    }
+//    private String queryInstallerPkgName(){
+//
+//        Intent intent = new Intent(PackageInstaller.ACTION_SESSION_DETAILS);
+//        List<ResolveInfo> resolveInfos = getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+//        if(resolveInfos == null || resolveInfos.size() == 0){
+//            return "";
+//        }
+//        return resolveInfos.get(0).activityInfo.packageName;
+//    }
     static final String TAG = "RecorderService";
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
@@ -40,15 +60,17 @@ public class Accessibility extends AccessibilityService{
 
     @Override
     protected void onServiceConnected() {
+
         super.onServiceConnected();
         AccessibilityServiceInfo info = new AccessibilityServiceInfo();
         info.eventTypes = AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED;
         info.packageNames = new String[]
-                {"com.example.admin.event", "com.android.settings" ,"com.android.packageinstaller"};
+                {"com.example.admin.event", querySettingPkgName() ,"com.android.packageinstaller"};
         info.feedbackType = AccessibilityServiceInfo.FEEDBACK_SPOKEN;
         info.notificationTimeout = 100;
         this.setServiceInfo(info);
         Log.d(TAG, "onServiceConnected");
+        //Log.d(TAG, queryInstallerPkgName());
     }
     //запускаем окно блокировки
     public void startLock(){
